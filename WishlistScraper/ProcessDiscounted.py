a4cc20e6f08f2116fd8f9404ba6294b961d1dc1f
TorvaMessor159 <mattloe67@gmail.com>
2020-07-01 16:22:25 +0800

    Started code to filter out non-discounted items

---
+++ WishlistScraper/ProcessDiscounted.py
@@ -0,0 +1,21 @@
+"""
+Author: Matthew Loe
+Date Last Modified: 1/7/2020
+Description: Processes scraped data to only discounted items
+"""
+
+def scrape_wishlist(data):
+    #Filters out non discounted items
+    
+    for key in data:
+        game_dict = data[key]
+        subs = game_dict['subs']
+        if subs:
+            subs = subs[0]
+            if not "no_discount" in subs['discount_block']:
+                print(subs)
+
+
+
+
+
