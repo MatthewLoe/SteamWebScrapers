3e6302522417825d90ffabf72e001c582982d458
TorvaMessor159 <mattloe67@gmail.com>
2020-07-01 19:28:02 +0800

    Finished filtering code

--- WishlistScraper/ProcessDiscounted.py
+++ WishlistScraper/ProcessDiscounted.py
@@ -1,3 +1,5 @@
+from decimal import Decimal
+
 """
 Author: Matthew Loe
 Date Last Modified: 1/7/2020
@@ -7,13 +9,32 @@ Description: Processes scraped data to only discounted items
 def processData(data):
     #Filters out non discounted items
     
+    items = list()
+
     for key in data:
         game_dict = data[key]
         subs = game_dict['subs']
         if subs:
             subs = subs[0]
-            if not "no_discount" in subs['discount_block']:
-                print(subs)
+            text = subs['discount_block']
+            if not "no_discount" in text:
+                item = {
+                    "name" : game_dict['name'],
+                    "appid" : key,
+                    "discount" : subs['discount_pct'],
+                }
+
+                #Find original price
+                result = text.find('A$')
+                item["original_price"] = text[result: result + 8]
+
+                #Find discount price
+                result = text.rfind('A$')
+                item["discount_price"] = text[result: result + 8]
+
+                items.append(item)
+
+    return items
 
 
 
