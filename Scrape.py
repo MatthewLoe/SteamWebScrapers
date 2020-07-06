e5c9fe9e1482434f244fab5a7de07b713e90c513
Matthew Loe <49096606+MatthewLoe@users.noreply.github.com>
2020-07-06 14:26:08 +0800

    Update Scrape.py
    Added the url variable with comment on how to edit and passed as parameter to get_wishlist

--- Scrape.py
+++ Scrape.py
@@ -6,6 +6,8 @@ Author: Matthew Loe
 Date Last Modified: 1/7/2020
 Description: Script for running scrape
 """
+#Edit url, replacing USERNAME with the user wishlist to be scraped  
+url = 'https://store.steampowered.com/wishlist/id/USERNAME/#sort=order'
 
-data = get_wishlist()
+data = get_wishlist(url)
 processData(data)
